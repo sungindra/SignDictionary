17e6ada111b2b32d74798238bee8169848dece8d
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-05-15 18:37:33 +0700

    Giving the first result

--- app/src/main/java/com/reynardvincent/signdictionary/Controller/MainActivity.kt
+++ app/src/main/java/com/reynardvincent/signdictionary/Controller/MainActivity.kt
@@ -6,7 +6,9 @@ import android.widget.Adapter
 import android.widget.ArrayAdapter
 import com.reynardvincent.signdictionary.Model.Category
 import com.reynardvincent.signdictionary.Model.Dictionary
+import com.reynardvincent.signdictionary.Model.SampleData
 import com.reynardvincent.signdictionary.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +17,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        categoryAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, SampleData.categories)
+
+        category_view.adapter = categoryAdapter
     }
 }
--- app/src/main/java/com/reynardvincent/signdictionary/Model/Model.kt
+++ app/src/main/java/com/reynardvincent/signdictionary/Model/Model.kt
@@ -6,6 +6,10 @@ class Model {
     var dictionaryURL = URL("")
 }
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
 
 class Dictionary(val key: String, val value: String, val image: String)
