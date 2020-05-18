5472572f4ff4a38ebe6013bc8a1ae2fef4c8c48a
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-05-18 20:01:06 +0700

    modify list item and adaptive data collection based on title

--- app/src/main/java/com/reynardvincent/signdictionary/Model/SampleData.kt
+++ app/src/main/java/com/reynardvincent/signdictionary/Model/SampleData.kt
@@ -9,7 +9,20 @@ object SampleData {
         Category("Sapaan", "Sapaan")
     )
 
-    var abjad = arrayListOf<Dictionary>()
+    var abjad = arrayListOf<Dictionary>(
+        Dictionary("A", "A", "A"),
+        Dictionary("B", "B", "B"),
+        Dictionary("C", "C", "C"),
+        Dictionary("D", "D", "D"),
+        Dictionary("E", "E", "E"),
+        Dictionary("F", "F", "F"),
+        Dictionary("G", "G", "G"),
+        Dictionary("H", "H", "H"),
+        Dictionary("I", "I", "I"),
+        Dictionary("J", "J", "J"),
+        Dictionary("K", "K", "K"),
+        Dictionary("L", "L", "L")
+    )
 
     var kataKerja = arrayListOf<Dictionary>()
 
@@ -18,4 +31,17 @@ object SampleData {
     var kataSifat = arrayListOf<Dictionary>()
 
     var sapaan = arrayListOf<Dictionary>()
+
+    var blank = arrayListOf<Dictionary>()
+
+    fun getDictionary(category: String) : List<Dictionary>{
+        return when(category){
+            "Abjad" -> abjad
+            "Kata Kerja" -> kataKerja
+            "Angka" -> angka
+            "Kata Sifat" -> kataSifat
+            "Sapaan" -> sapaan
+            else -> blank
+        }
+    }
 }
