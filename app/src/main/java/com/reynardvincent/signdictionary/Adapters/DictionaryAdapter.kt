f190358f5cd9bd30233f1cbbb25a4eb5cc46c8dc
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-05-18 23:38:20 +0700

    completed the searched view activity and added adaptable grid display adapter based on devices

---
+++ app/src/main/java/com/reynardvincent/signdictionary/Adapters/DictionaryAdapter.kt
@@ -0,0 +1,41 @@
+package com.reynardvincent.signdictionary.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.reynardvincent.signdictionary.Model.Dictionary
+import com.reynardvincent.signdictionary.R
+
+class DictionaryAdapter(val context: Context, val dictionaries: List<Dictionary>) : RecyclerView.Adapter<DictionaryAdapter.DictionaryHolder>(){
+
+    inner class DictionaryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val dictionaryImage = itemView.findViewById<ImageView>(R.id.signImage)
+        val dictionaryTitle = itemView.findViewById<TextView>(R.id.signTitle)
+        val dictionaryDetail = itemView.findViewById<TextView>(R.id.signDetail)
+
+        fun bindDictionary(dictionary: Dictionary, context: Context){
+            //val resourceID = context.resources.getIdentifier(dictionary.image, "drawable", context.packageName)
+            val resourceID = context.resources.getIdentifier("ic_launcher_background", "drawable", context.packageName)
+            dictionaryImage?.setImageResource(resourceID)
+            dictionaryTitle?.text = dictionary.key
+            dictionaryDetail?.text = dictionary.value
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DictionaryHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.dictionary_list_item, parent, false)
+        return DictionaryHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dictionaries.count()
+    }
+
+    override fun onBindViewHolder(holder: DictionaryHolder, position: Int) {
+        holder.bindDictionary(dictionaries[position], context)
+    }
+}
--- app/src/main/java/com/reynardvincent/signdictionary/Controller/SearchedView.kt
+++ app/src/main/java/com/reynardvincent/signdictionary/Controller/SearchedView.kt
@@ -1,12 +1,20 @@
 package com.reynardvincent.signdictionary.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.reynardvincent.signdictionary.Adapters.DictionaryAdapter
+import com.reynardvincent.signdictionary.Model.Dictionary
+import com.reynardvincent.signdictionary.Model.SampleData
 import com.reynardvincent.signdictionary.Model.extraCategory
 import com.reynardvincent.signdictionary.R
 import kotlinx.android.synthetic.main.activity_searched_view.*
 
 class SearchedView : AppCompatActivity() {
+
+    lateinit var dictionaryAdapter : DictionaryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +22,24 @@ class SearchedView : AppCompatActivity() {
 
         val categoryType = intent.getStringExtra(extraCategory)
         TypeTitle.text = categoryType
+        dictionaryAdapter = DictionaryAdapter(this, SampleData.getDictionary(categoryType))
+
+        var numColumn = 2
+        val orientation = resources.configuration.orientation
+        val screenSize = resources.configuration.screenWidthDp
+
+        if(screenSize <= 720){
+            if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                numColumn = 3
+            } else {
+                numColumn = 2
+            }
+        } else {
+            numColumn = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, numColumn)
+        dictionaryListView.layoutManager = layoutManager
+        dictionaryListView.adapter = dictionaryAdapter
     }
 }
