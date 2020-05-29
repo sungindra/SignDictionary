242dbac1c331a6629a58d683740a7d3ae1570b0c
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-05-29 20:24:15 +0700

    Making the data setup for non web request

--- app/src/main/java/com/reynardvincent/signdictionary/Model/SampleData.kt
+++ app/src/main/java/com/reynardvincent/signdictionary/Model/SampleData.kt
@@ -21,7 +21,22 @@ object SampleData {
         Dictionary("I", "I", "I"),
         Dictionary("J", "J", "J"),
         Dictionary("K", "K", "K"),
-        Dictionary("L", "L", "L")
+        Dictionary("L", "L", "L"),
+        Dictionary("M", "M", "M"),
+        Dictionary("N", "N", "N"),
+        Dictionary("O", "O", "O"),
+        Dictionary("P", "P", "P"),
+        Dictionary("Q", "Q", "Q"),
+        Dictionary("R", "R", "R"),
+        Dictionary("S", "S", "S"),
+        Dictionary("T", "L", "L"),
+        Dictionary("U", "U", "U"),
+        Dictionary("V", "V", "V"),
+        Dictionary("W", "W", "W"),
+        Dictionary("X", "X", "X"),
+        Dictionary("Y", "Y", "Y"),
+        Dictionary("Z", "Z", "Z")
+
     )
 
     var kataKerja = arrayListOf<Dictionary>()
